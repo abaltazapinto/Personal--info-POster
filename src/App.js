3ee339be1b0ffd687ef651007757ceaa10923357
abaltaza <andrebaltazarpinto@gmail.com>
2024-05-29 17:28:52 +0100

    done background

--- src/App.js
+++ src/App.js
@@ -5,32 +5,20 @@ import About from './Components/About';
 import Interests from './Components/Interests';
 import './style.css'
 import Footer from './Components/Footer';
-import fotografia from './img/IMG_20191231_123456.jpg'
+
 
 function App() {
-  const backgroundStyle = {
-    backgroundImage: `url(${fotografia})`,
-    bacgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    height: '100vh',
-    width: '100%',
-    margin: '0',
-    fontfamily: 'Arial, sans-serif',
-  };
-
   return (
     <>
-    <div style={backgroundStyle}>
       <div className='Main'>
         <div className='Main2'>
           <Initial />
           <About />
           <Interests />
           <Footer />
+       </div>
       </div>
-    </div>
-    </div>
+    
     </>
   );
 }
